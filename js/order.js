fc170ec1c448304f2796a610e07938e251867657
Temuulen918 <112910669+Temuulen918@users.noreply.github.com>
2023-11-14 23:44:45 +0800

    input zasav
    zahialgiin inputuuded shalgalt taviv

--- js/order.js
+++ js/order.js
@@ -2,36 +2,77 @@
         var calendarEl = document.getElementById('calendar');
             
         var calendar = new FullCalendar.Calendar(calendarEl, {
-            initialView: 'dayGridMonth', // Display a monthly view
+            initialView: 'dayGridMonth', 
             events: [
                 {title: 'Event 1', start: '2023-11-01'}
             ]
         });
 
-        calendar.render(); // Render the calendar
+        calendar.render();
     });
 
 
     document.addEventListener('DOMContentLoaded', function () {
-        // Set default room number to 201
+        
         let selectedRoomNumber = '201';
-        document.getElementById('selectedRoom').innerText = selectedRoomNumber;
 
-        // Handle button clicks
-        const roomButtons = document.querySelectorAll('#button-grid button');
+        const roomButtons = document.querySelectorAll('#button-grid div button');
         roomButtons.forEach(button => {
             button.addEventListener('click', function () {
-                // Update selected room number
+    
                 selectedRoomNumber = this.innerText;
                 document.getElementById('selectedRoom').innerText = selectedRoomNumber;
 
-                // Remove 'selected' class from all buttons
                 roomButtons.forEach(btn => btn.classList.remove('selected'));
 
-                // Add 'selected' class to the clicked button
                 this.classList.add('selected');
             });
         });
+        
+        document.getElementById('btn201').classList.add('selected');
+    });
 
-        // Your other JavaScript code
+    document.addEventListener('DOMContentLoaded', function () {
+       
+        const startDateInput = document.getElementById('startDateInput');
+        const endDateInput = document.getElementById('endDateInput');
+    
+        startDateInput.addEventListener('input', function () {
+           
+            const today = new Date().toISOString().split('T')[0]; 
+            if (this.value < today) {
+                this.value = today;
+            }
+    
+            endDateInput.min = this.value;
+    
+            if (endDateInput.value < this.value) {
+                endDateInput.value = this.value;
+            }
+        });
+        
+    });
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const lnameInput = document.getElementById('lname');
+        const phoneInput = document.getElementById('phone');
+        const fnameInput = document.getElementById('fname');
+        const registerNoInput = document.getElementById('registerNo');
+    
+        lnameInput.addEventListener('input', function () {
+            this.value = this.value.replace(/[^a-zA-Z-]/g, '');
+        });
+    
+        phoneInput.addEventListener('input', function () {
+            this.value = this.value.replace(/\D/g, '');
+        });
+    
+        fnameInput.addEventListener('input', function () {
+            this.value = this.value.replace(/[^a-zA-Z-]/g, '');
+        });
+
+        registerNoInput.addEventListener('input', function () {
+            this.value = this.value.replace(/[^a-zA-Z0-9]/g, '');
+
+        });
     });
